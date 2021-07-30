ad50b51929bc0c01f21193df0be03f23a99ad4fd
hongm <hongm@user>
2021-07-30 15:53:16 +0900

    테스트

---
+++ Java 첫프로젝트/src/project배달/Main.java
@@ -0,0 +1,181 @@
+package project배달;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		User user = new User();
+		
+		Shop shop = new Shop();
+		Food food = new Food();
+		Order order = new Order();
+		SQL sql = new SQL();
+		
+		boolean run = true;
+		int menu = 0;
+		String u_id = null;
+		int u_point = 0;
+		
+		sql.connect();
+		sql.orderAll();
+		
+		do {
+			System.out.println("==================================");
+			System.out.println("1.회원가입          2.로그인          3.IDㆍPW찾기");
+			System.out.println("==================================");
+			System.out.print("메뉴 선택 >> ");
+			
+			menu = sc.nextInt();
+			 
+			switch(menu) {
+			case 1:			//회원 가입
+				System.out.print("아이디 >> ");
+				u_id = sc.next();
+				
+				System.out.print("비밀번호(4자리) >> ");
+				int u_pw = sc.nextInt();
+				
+				System.out.print("이름 >> ");
+				String u_name = sc.next();
+				
+				System.out.print("주소 >> ");
+				sc.nextLine();
+				String u_addr = sc.nextLine();
+				
+				
+				System.out.print("핸드폰번호 >> ");
+				String u_phone = sc.next();
+				
+				System.out.print("생년월일(ex. 20210727) >> ");
+				String u_birth = sc.next();
+				
+				user.setU_id(u_id);
+				user.setU_pw(u_pw);
+				user.setU_name(u_name);
+				user.setU_addr(u_addr);
+				user.setU_phone(u_phone);
+				user.setU_birth(u_birth);
+				
+				sql.insert(user);
+				break;
+			case 2:		//로그인
+				System.out.print("아이디 >> ");
+                u_id = sc.next();
+
+                System.out.print("비밀번호(4자리) >> ");
+                u_pw = sc.nextInt();
+
+                boolean check = sql.idCheck(u_id,u_pw);	
+
+                if(check) {
+                    System.out.println("로그인 했습니다.");  
+                    sql.shopname();
+                    sql.pointC(u_id);
+                    System.out.println("");
+                    run = false;
+                }else {
+                    System.out.println("로그인 실패 ");
+                }
+				break;
+			case 3:
+				System.out.print("1.아이디 찾기  2.비밀번호 찾기 >> ");
+				int num = sc.nextInt();
+				
+				if(num == 1) {
+					System.out.print("이름 >> ");
+					u_name = sc.next();
+					
+					System.out.print("생년월일 >> ");
+					u_birth = sc.next();
+					
+					boolean check1 = sql.nbCheck(u_name,u_birth);
+		
+					
+				} else if(num == 2) {
+					System.out.print("아이디 >> ");
+					u_id = sc.next();
+					
+					System.out.print("이름 >> ");
+					u_name = sc.next();
+					
+					System.out.print("생년월일 >> ");
+					u_birth = sc.next();
+					
+					boolean check2 = sql.inbCheck(u_id,u_name,u_birth);
+				} else {
+					System.out.println("잘못 입력하셨습니다.");
+				}
+				
+				break;
+				
+			default :
+				System.out.println("잘못 입력하셨습니다.");
+				break;
+			}
+			
+			
+		} while(run);
+		
+		
+		menu = 0;
+		run = true;
+		
+		do {
+			System.out.println("==========================================================");
+			System.out.println("1.음식 이름으로 찾기          2.가격대로 찾기          3.가게별 찾기          4.포인트 적립");
+			System.out.println("5.장바구니                       6.결제하기                7.종료");
+			System.out.println("==========================================================");
+			System.out.print("메뉴 선택 >> ");
+			
+			menu = sc.nextInt();
+			
+			switch(menu) {
+			case 1:			// 음식 이름으로 찾기
+				sql.searchN();
+				break;
+			case 2:			// 가격대로 찾기
+				System.out.println("원하시는 가격대를 선택해주세요!");
+				System.out.println("1. 전체   2. 10,000원 이하   3. 10,000~15,000   4. 15,000~20,000");
+				int sel = sc.nextInt();
+				sql.searchP(sel);
+				break;
+			case 3:			// 가게별 찾기
+				System.out.println("찾으실 가게 이름을 입력해 주세요!");
+				String shopname = sc.next();
+				sql.searchS(shopname);
+				break;
+			case 4:
+				sql.pointC(u_id);
+				sql.pointS(u_id);
+				
+				break;
+			case 5:			// 장바구니  -> 음식 취소 포함
+				sql.showOrder();
+				sql.dropOrder();
+				break;
+			case 6:			// 결제하기	
+				sql.checkout(u_id);
+				run = false;
+				break;
+			case 7:			// 종료
+				sql.quit();
+				run = false;
+				break;
+			default:
+				System.out.println("메뉴를 다시 선택해주세요");
+				break;
+			}
+			
+		} while(run);
+		
+		
+		
+		
+	}
+
+}
+
+
---
+++ Java 첫프로젝트/src/project배달/SQL.java
@@ -0,0 +1,569 @@
+package project배달;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class SQL {
+	Scanner sc = new Scanner(System.in);
+
+	// 1. DB접속
+	Connection con = null;
+
+	// 2. DB데이터 보내기
+	Statement stmt = null;
+	PreparedStatement pstmt = null; // ? 가 '문자'로 인식
+
+	// 3. DB데이터 받기
+	ResultSet rs = null;
+	ResultSet rs2 = null;
+
+	User user = new User();
+	
+	// [1} DB접속
+	public void connect() {
+		con = DBC.DBConnect();
+
+	}
+
+	public void insert(User user) {
+		String sql = "INSERT INTO P_USER VALUES(?,?,?,?,?,TO_DATE(?),?)";
+
+		try {
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, user.getU_id());
+			pstmt.setInt(2, user.getU_pw());
+			pstmt.setString(3, user.getU_name());
+			pstmt.setString(4, user.getU_addr());
+			pstmt.setString(5, user.getU_phone());
+			pstmt.setString(6, user.getU_birth());
+			pstmt.setInt(7, 0);
+
+			int result = pstmt.executeUpdate();
+
+			if (result > 0) {
+				System.out.println("회원가입 성공!");
+			} else {
+				System.out.println("회원가입 실패!");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public boolean idCheck(String u_id, int u_pw) {
+		boolean result = false;
+
+		String sql = "SELECT * FROM P_USER WHERE U_ID = ? AND U_PW = ?";
+
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, u_id);
+			pstmt.setInt(2, u_pw);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) { // SELECT문의 결과가 존재한다면
+				result = true;
+			} else { // 존재하지 않는다면
+				result = false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public boolean nbCheck(String u_name, String u_birth) {
+
+		boolean result = false;
+
+		String sql = "SELECT U_ID FROM P_USER WHERE U_NAME = ? AND U_BIRTH = TO_DATE(?)";
+
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, u_name);
+			pstmt.setString(2, u_birth);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				result = true;
+				System.out.println("아이디는 " + rs.getString(1));
+			} else {
+				System.out.println("입력한 회원정보에 일치하는 아이디가 없습니다.");
+				result = false;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public boolean inbCheck(String u_id, String u_name, String u_birth) {
+		boolean result = false;
+
+		String sql = "SELECT U_PW FROM P_USER WHERE U_ID = ? AND U_NAME = ? AND U_BIRTH = TO_DATE(?)";
+
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, u_id);
+			pstmt.setString(2, u_name);
+			pstmt.setString(3, u_birth);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				result = true;
+				System.out.println("비밀번호는 " + rs.getString(1));
+			} else {
+				System.out.println("입력한 회원정보에 일치하는 비밀번호가 없습니다.");
+				result = false;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public void shopname() {
+
+		String sql = "SELECT S_NAME, S_PHONE FROM SHOP";
+ 
+		try {
+			stmt = con.createStatement();
+
+			rs = stmt.executeQuery(sql);
+
+			
+			System.out.println("================================================");
+
+			while (rs.next()) {
+				System.out.print("가게이름 : " + rs.getString(1) + "          \t");
+				System.out.println("가게번호 : " + rs.getString(2));
+			}
+				
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void searchN() {
+		String sql = "SELECT * FROM FOOD WHERE F_NAME LIKE '%'||?||'%'";
+		System.out.println("찾으실 음식 이름을 입력해 주세요!");
+		String name = sc.next();
+		try {
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, name);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				System.out.print("가게이름 : " + rs.getString(1) + "\t");
+				System.out.print("음식이름 : " + rs.getString(2) + "\t");
+				System.out.println("가격 : " + rs.getString(3));
+			}
+
+			if(rs.getRow()<=0) {
+				System.out.println("검색 결과가 없습니다.");
+			}
+			else {
+				selectF();//음식 주문 메소드 호출
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void searchP(int sel) {
+		// TODO Auto-generated method stub
+		boolean run = true;
+		int min = 0;
+		int max = 0;
+		do {
+		
+		switch(sel)
+		{
+		case 1:
+			min = 0;
+			max = 50000;
+			run = false;
+			break;
+		case 2:
+			min = 0;
+			max = 10000;
+			run = false;
+			break;
+		case 3:
+			min = 10000;
+			max = 15000;
+			run = false;
+			break;
+		case 4:
+			min = 15000;
+			max = 20000;
+			run = false;
+			break;
+			default:
+				System.out.println("다시선택해주세요");
+				break;
+			
+		}
+		}while(run);
+		String sql = "SELECT * FROM FOOD WHERE F_PRICE BETWEEN ? and ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, min);
+			pstmt.setInt(2, max);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				System.out.print("가게이름 : " + rs.getString(1) + "\t");
+				System.out.print("음식이름 : " + rs.getString(2) + "\t");
+				System.out.println("가격 : " + rs.getString(3));
+			}
+
+			if(rs.getRow()<=0) {
+				System.out.println("검색 결과가 없습니다.");
+			}
+			else {
+				selectF();//음식 주문 메소드 호출
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void searchS(String shopname) {
+		// TODO Auto-generated method stub
+		String sql = "SELECT * FROM FOOD WHERE F_SNAME LIKE '%'||?||'%'";
+		try {
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, shopname);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				System.out.print("가게이름 : " + rs.getString(1) + "\t");
+				System.out.print("음식이름 : " + rs.getString(2) + "\t");
+				System.out.println("가격 : " + rs.getString(3));
+			}
+
+			if(rs.getRow()<=0) {
+				System.out.println("검색 결과가 없습니다.");
+			}
+			else {
+				selectF();//음식 주문 메소드 호출
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void quit() {
+		// TODO Auto-generated method stub
+		System.out.println("이용해 주셔서 감사합니다.");
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void selectF() {
+		// TODO Auto-generated method stub
+		boolean run = true;
+		while (run) {
+			System.out.println("더 주문 하실 음식이 있다면 '1' 아니면 '2' 를 입력해주세요");
+
+			int num = sc.nextInt();
+			switch (num) {
+			case 1:
+				System.out.println("주문하실 음식을 정확하게 입력해 주세요.");
+				String name = sc.next();
+				String sql = "SELECT F_NAME, F_PRICE FROM FOOD WHERE F_NAME LIKE ?";
+				try {
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, name);
+					rs = pstmt.executeQuery();
+
+					if (rs.next()) {
+						String resultName = rs.getString(1);
+						int resultPrice = rs.getInt(2);
+						insertFood(resultName, resultPrice);
+					} else {
+						System.out.println("품목이 없습니다. 다시 확인해 주세요.");
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 2:
+				System.out.println("장바구니에 담기를 종료합니다.");
+				run = false;
+				break;
+			default:
+				System.out.println("숫자를 다시 입력해주세요");
+				break;
+			}
+		}
+	}
+
+	public void insertFood(String rn, int rp) {
+
+		String sql = "INSERT INTO P_ORDER VALUES(?,?)";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, rn);
+			pstmt.setInt(2, rp);
+			int result = pstmt.executeUpdate();
+			if (result > 0) {
+				System.out.println(rn + "을 넣었습니다. 가격은 : " + rp);
+			} else {
+				System.out.println("장바구니에 담기 실패하였습니다.");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void showOrder() {
+		// TODO Auto-generated method stub
+		String sql = "select * from P_ORDER";
+		int sum = 0;
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			System.out.println("주문하신 음식은");
+			while (rs.next()) {
+				System.out.print(rs.getString(1) + "\t");
+				System.out.println("가격은 : " + rs.getInt(2));
+				sum += rs.getInt(2);
+			}
+			System.out.println("총 금액은 " + sum);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void dropOrder() {
+		// TODO Auto-generated method stub
+		System.out.println("장바구니 품목을 취소 하시겠습니까? 1. Y 2. N");
+		int num = sc.nextInt();
+		switch (num) {
+		case 1:
+			System.out.println("1. 전체 취소      2. 부분 취소    3.종료");
+			int numInside = sc.nextInt();
+			switch (numInside) {
+			case 1:
+				orderAll();
+				break;
+			case 2:
+				System.out.println("취소할 음식을 정확하게 입력해주세요.");
+				String name = sc.next();
+				orderPart(name);
+				break;
+			case 3:
+				System.out.println("메뉴로 돌아갑니다.");
+				break;
+			default:
+				System.out.println("다시선택해주세요.");
+				break;
+			}
+			break;
+		case 2:
+
+			break;
+		default:
+			System.out.println("다시선택해주세요.");
+			break;
+		}
+	}
+
+	public void orderAll() {
+		String sql1 = "DROP TABLE P_ORDER";
+		String sql2 = "CREATE TABLE P_ORDER( O_NAME NVARCHAR2(10) NOT NULL, O_PRICE NUMBER NOT NULL)";
+
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql1);
+			rs = stmt.executeQuery(sql2);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void orderPart(String name) {
+		String sql = "DELETE P_ORDER WHERE O_NAME=?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void checkout(String u_id) {
+		// TODO Auto-generated method stub
+		System.out.println("결제 방법을 선택해주세요.");
+		System.out.println("1. 무통장 입금    2. 현장결제    3.포인트로 결제 (그외는 메뉴로 갑니다.)");
+		int num = sc.nextInt();
+		switch (num) {
+		case 1:
+			checkOut();
+			quit();
+			break;
+		case 2:
+			quit();
+			break;
+		case 3:
+			checkPoint(u_id);
+			break;
+		default:
+			System.out.println("메뉴로 갑니다.");
+			break;
+		}
+	}
+	
+	public void checkOut() {
+		String sql = "Select distinct(S_Account) from shop";
+		showOrder();
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+				System.out.println(rs.getString(1) + "계좌에 입금 해주십쇼.");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	public void checkPoint(String u_id) {
+		String sql1 = "SELECT U_POINT FROM P_USER WHERE U_ID = ?";
+		String sql2 = "select O_PRICE from P_ORDER";
+		int sum = 0;
+		int balance = 0;
+		try {
+			pstmt = con.prepareStatement(sql1);
+			pstmt.setString(1, u_id);
+			rs = pstmt.executeQuery();
+			stmt = con.createStatement();
+			rs2 = stmt.executeQuery(sql2);
+			while(rs2.next())
+				 sum += rs2.getInt(1);
+			if(rs.next())
+			{
+				balance = rs.getInt(1);
+			}
+			if(sum>balance)
+			{
+				System.out.println("포인트가 부족합니다!");
+				quit();
+			}
+			else
+			{
+				discount(sum, u_id);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void discount(int sum,String u_id) {
+		// TODO Auto-generated method stub
+		String sql = "UPDATE P_USER SET U_POINT = U_POINT - ? WHERE U_ID = ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, sum);
+			pstmt.setString(2, u_id);
+			pstmt.executeUpdate();
+			System.out.println(sum+"결재 되었습니다.");
+			quit();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void pointS(String u_id) {
+		// TODO Auto-generated method stub
+		System.out.println("얼마나 적립하시겠습니까?");
+		int point = sc.nextInt();
+		String sql = "UPDATE P_USER SET U_POINT = U_POINT + ? WHERE U_ID = ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, point);
+			pstmt.setString(2, u_id);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void pointC(String u_id) {
+		// TODO Auto-generated method stub
+		String sql = "SELECT U_POINT FROM P_USER WHERE U_ID = ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, u_id);
+			rs = pstmt.executeQuery();
+			if(rs.next())
+			{
+				int num = rs.getInt(1);
+				System.out.println(u_id+"   님의 잔액은   "+num);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
